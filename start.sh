3946f2cc80f4ffedb57ade8ac5b1bc400eea041c
Caaarlowsz <caaarlowsz@outlook.com>
2024-12-28 20:17:27 -0300

    Base server of pvp-classic project.

---
+++ start.sh
@@ -0,0 +1,2 @@
+clear
+java -jar spigot-1.8.8-R0.1-SNAPSHOT.jar --universe worlds --config configs/server.properties --bukkit-settings configs/bukkit.yml --commands-settings configs/commands.yml --spigot-settings configs/spigot.yml
